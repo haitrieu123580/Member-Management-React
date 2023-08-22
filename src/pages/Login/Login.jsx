f63cb580307cbb4b215f01116d33223b370c31c3
haitrieu123580 <haitrieu123580@gmail.com>
2023-08-22 10:07:22 +0700

    redux toolkit

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,43 +1,67 @@
 import React from 'react'
-import axios from "axios";
-import PropTypes from 'prop-types';
-import { useState } from "react";
+// import axios from "axios";
+// import PropTypes from 'prop-types';
+// import { useState } from "react";
 import { useNavigate } from 'react-router-dom';
-import { useAuth } from '../../context/Auth';
+// import { useAuth } from '../../context/Auth';
+import { useForm } from 'react-hook-form'
+import { useDispatch, useSelector } from 'react-redux'
+import { userLogin } from '../../features/auth/authAction'
+import { useEffect } from 'react'
+import Error from '../../components/Error/Error';
+import Spinner from '../../components/Spinner/Spinner';
 const Login = () => {
-  const { setToken, setUser } = useAuth();
-  const [credentials, setCredentials] = useState({
-    username: undefined,
-    password: undefined,
-  });
-  const handleChange = (e) => {
-    setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
-  };
-  const navigate = useNavigate();
-  const handleClick = async (e) => {
-    e.preventDefault();
-    try {
-      const res = await axios.post("http://localhost:3000/auth/login", credentials);
-      console.log(res.data.message)
-      if (res.status === 200) {
-        setToken(res.data.accessToken);
-        setUser(res.data.message);
-        localStorage.setItem('accessToken', res.data.accessToken)
-        navigate('/home');       
-      }
-      else{
-        navigate('/')
-      }
+  // const { setToken, setUser } = useAuth();
+  // const [credentials, setCredentials] = useState({
+  //   username: undefined,
+  //   password: undefined,
+  // });
+  // const handleChange = (e) => {
+  //   setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  // };
+  // const navigate = useNavigate();
+  // const handleClick = async (e) => {
+  //   e.preventDefault();
+  //   try {
+  //     const res = await axios.post("http://localhost:3000/auth/login", credentials);
+  //     console.log(res.data.message)
+  //     if (res.status === 200) {
+  //       setToken(res.data.accessToken);
+  //       setUser(res.data.message);
+  //       localStorage.setItem('accessToken', res.data.accessToken)
+  //       navigate('/home');       
+  //     }
+  //     else{
+  //       navigate('/')
+  //     }
 
-    } catch (err) {
-      console.log(err)
+  //   } catch (err) {
+  //     console.log(err)
+  //   }
+  // };
+  const { loading, userInfo, error } = useSelector((state) => state.auth)
+  const dispatch = useDispatch()
+
+  const { register, handleSubmit } = useForm()
+
+  const navigate = useNavigate()
+
+  // redirect authenticated user to profile screen
+  useEffect(() => {
+    if (userInfo) {
+      navigate('/dashboard')
     }
-  };
+  }, [navigate, userInfo])
+
+  const submitForm = (data) => {
+    dispatch(userLogin(data))
+  }
   return (
     <div className='h-screen flex justify-center items-center'>
       <div className="bg-white p-8 rounded shadow-md w-96 h-fit">
         <h1 className="text-2xl font-semibold mb-4">Login</h1>
-        <form>
+        {error && <Error>{error}</Error>}
+        <form onSubmit={handleSubmit(submitForm)}>
           <div className="mb-4">
             <label htmlFor="username" className="block text-sm font-medium text-gray-700">Username</label>
             <input
@@ -46,7 +70,8 @@ const Login = () => {
               name="username"
               className="text-black mt-1 px-3 py-2 w-full border rounded-lg focus:ring focus:ring-indigo-200"
               placeholder="Enter your username"
-              onChange={handleChange} />
+              {...register('username')}
+            />
           </div>
           <div className="mb-4">
             <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
@@ -56,13 +81,15 @@ const Login = () => {
               name="password"
               className="text-black mt-1 px-3 py-2 w-full border rounded-lg focus:ring focus:ring-indigo-200"
               placeholder="Enter your password"
-              onChange={handleChange} />
+              {...register('password')}
+            />
           </div>
           <button
-            onClick={handleClick}
             type="submit"
-            className="w-full bg-indigo-500 text-white rounded-lg py-2 hover:bg-indigo-600 focus:outline-none focus:ring focus:ring-indigo-200">
-            Log in
+            className="w-full bg-indigo-500 text-white rounded-lg py-2 hover:bg-indigo-600 focus:outline-none focus:ring focus:ring-indigo-200"
+            disabled={loading}
+          >
+            {loading ? <Spinner /> : 'Login'}
           </button>
         </form>
       </div>
@@ -71,8 +98,8 @@ const Login = () => {
   )
 }
 
-Login.propTypes = {
-  setToken: PropTypes.func.isRequired
-};
+// Login.propTypes = {
+//   setToken: PropTypes.func.isRequired
+// };
 
 export default Login
